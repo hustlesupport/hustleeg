46c82e3cb67f13873af3e1976a70e0b6913f566a
youssef mohamed <youssefhusseinnn1@gmail.com>
2026-07-22 19:34:29 +0300

    add

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "fastly.picsum.photos" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "ibb.co" },
     ],
     formats: ["image/avif", "image/webp"],
   },
